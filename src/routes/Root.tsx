bd29f1ca14784e85e4179387533eebadf916a3e0
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-04-22 21:35:13 +0900

    feat(#1): 추가 html, css Templates, 추가 Bootstrap CDN

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,5 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 export function Root() {
-  return <Outlet />;
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
 }
