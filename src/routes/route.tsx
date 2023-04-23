e60d940c23b03b141119d14e0d09cd6bd1d98124
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-04-23 14:51:53 +0900

    fix(#9): 수정 SigIn -> SignIn

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -7,7 +7,7 @@ import ErrorPage from '../pages/ErrorPage';
 import HomePage from '../pages/HomePage/HomePage';
 import ProfilePage from '../pages/ProfilePage/ProfilePage';
 import SettingsPage from '../pages/SettingsPage/SettingsPage';
-import SignInPage from '../pages/SigInPage/SignInPage';
+import SignInPage from '../pages/SignInPage/SignInPage';
 import SignUpPage from '../pages/SignUpPage/SignUpPage';
 import MyAricles from '../pages/ProfilePage/MyAricles';
 import FavoritedArticles from '../pages/ProfilePage/FavoritedArticles';
