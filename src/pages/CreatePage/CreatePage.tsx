9b0db12130eaa8a36164f80d7098b1b10382a2cc
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-05-04 10:16:31 +0900

    feat(#5): 추가 useInputs, tag

--- src/pages/CreatePage/CreatePage.tsx
+++ src/pages/CreatePage/CreatePage.tsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import { useState } from 'react';
+import useInputs from '../../hooks/useInputs';
 
-function CreatePage() {
+function CreatePage(): JSX.Element {
+  const [form, onChange] = useInputs({
+    title: '',
+    description: '',
+    body: '',
+  });
+  const { title, description, body } = form;
+
+  const [tag, setTag] = useState('');
+  const [tagList, setTagList] = useState<string[]>([]);
+
+  const handleKeyDown = (
+    event: React.KeyboardEvent<HTMLInputElement>,
+  ): void => {
+    if (event.key === 'Enter') {
+      setTagList((prev) => [tag, ...prev]);
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setTag(e.target.value);
+  };
+
   return (
     <div className="editor-page">
       <div className="container page">
@@ -13,6 +36,9 @@ function CreatePage() {
                     type="text"
                     className="form-control form-control-lg"
                     placeholder="Article Title"
+                    name="title"
+                    value={title}
+                    onChange={onChange}
                   />
                 </fieldset>
                 <fieldset className="form-group">
@@ -20,6 +46,9 @@ function CreatePage() {
                     type="text"
                     className="form-control"
                     placeholder="What's this article about?"
+                    name="description"
+                    value={description}
+                    onChange={onChange}
                   />
                 </fieldset>
                 <fieldset className="form-group">
@@ -27,15 +56,27 @@ function CreatePage() {
                     className="form-control"
                     rows={8}
                     placeholder="Write your article (in markdown)"
-                  ></textarea>
+                    name="body"
+                    value={body}
+                    onChange={onChange}
+                  />
                 </fieldset>
                 <fieldset className="form-group">
                   <input
                     type="text"
                     className="form-control"
                     placeholder="Enter tags"
+                    name="tag"
+                    value={tag}
+                    onChange={handleChange}
+                    onKeyDown={handleKeyDown}
                   />
-                  <div className="tag-list"></div>
+                  <div className="tag-list">
+                    <span className="tag-default tag-pill ng-binding ng-scope">
+                      <i className="ion-close-round" />
+                      et
+                    </span>
+                  </div>
                 </fieldset>
                 <button
                   className="btn btn-lg pull-xs-right btn-primary"
