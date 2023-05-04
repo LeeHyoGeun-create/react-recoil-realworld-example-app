aeb6254d6e555d98315bb0561eab30643614ea21
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-05-04 23:40:17 +0900

    refactor(#5): 변경 interface

--- src/util/formValidation.ts
+++ src/util/formValidation.ts
@@ -1,16 +1,26 @@
 import isEmpty from './isEmpty';
 
-const areAllFieldsFilled = (
-  formObj: Record<string, string>,
-): [boolean, Record<string, string>] => {
-  let newError = {};
+type FormFields = Record<string, string>;
+
+type FormErrors = Record<string, string>;
+
+interface ValidationResult {
+  isValid: boolean;
+  errors: FormErrors;
+}
+
+const areAllFieldsFilled = (formObj: FormFields): ValidationResult => {
+  let newError: FormErrors = {};
   for (const [key, value] of Object.entries(formObj)) {
     if (isEmpty(value)) {
       newError = { ...newError, [key]: "can't be blank" };
       break;
     }
   }
-  return [Object.keys(newError).length === 0, newError];
+  return {
+    isValid: Object.keys(newError).length === 0,
+    errors: newError,
+  };
 };
 
 export default areAllFieldsFilled;
