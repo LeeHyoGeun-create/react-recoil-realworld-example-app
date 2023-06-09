03b05f879f0973faea54b994fb5b526d41365927
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-06-09 15:32:55 +0900

    feat(#5): 추가 값 수정 기능

--- src/hooks/useInputs.ts
+++ src/hooks/useInputs.ts
@@ -1,27 +1,35 @@
 import type React from 'react';
 import { useState, useCallback } from 'react';
 
-function useInputs<T extends Record<string, any>>(
+function useInputs<T extends Record<string, string>>(
   initialForm: T,
 ): [
   T,
-  (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void,
+  (event: React.ChangeEvent<HTMLInputElement>) => void,
   () => void,
+  (name: string, value: string) => void,
+  (newState: T) => void,
 ] {
   const [form, setForm] = useState<T>(initialForm);
   const reset = useCallback(() => {
     setForm(initialForm);
   }, [initialForm]);
 
-  const onChange = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ): void => {
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const { name, value } = event.target;
 
     setForm((preState) => ({ ...preState, [name]: value }));
   };
 
-  return [form, onChange, reset];
+  const setValue = (name: string, value: string): void => {
+    setForm((preState) => ({ ...preState, [name]: value }));
+  };
+
+  const setValues = (newSatate: T): void => {
+    setForm((preState) => ({ ...preState, ...newSatate }));
+  };
+
+  return [form, onChange, reset, setValue, setValues];
 }
 
 export default useInputs;
